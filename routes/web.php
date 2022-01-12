686cf5ea2fce03beb448f2fb3ed57375123ce239
Andika Bahari <kzlfxx@gmail.com>
2022-01-12 21:58:51 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PesananController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\PenggunaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +26,28 @@ Route::post('/login', LoginController::class)->name('login');
 
 Route::get('/logout', LogoutController::class)->name('logout');
 
-Route::get('/dashboard', DashboardController::class)->name('dashboard')->middleware('myauth.user');
+Route::middleware(['myauth.user'])->group(function ()
+{
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
+    Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan');
+    Route::get('/pesanan/create', [PesananController::class, 'create'])->name('pesanan.create');
+    Route::post('/pesanan/store', [PesananController::class, 'store'])->name('pesanan.store');
+    Route::get('/pesanan/edit', [PesananController::class, 'edit'])->name('pesanan.edit');
+    Route::put('/pesanan/update', [PesananController::class, 'update'])->name('pesanan.update');
+    Route::delete('/pesanan/delete', [PesananController::class, 'destroy'])->name('pesanan.delete');
 
+    Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+    Route::get('/menu/create', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('/menu/store', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('/menu/edit', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::put('/menu/update', [MenuController::class, 'update'])->name('menu.update');
+    Route::delete('/menu/delete', [MenuController::class, 'destroy'])->name('menu.delete');
+
+    Route::get('/pengguna', [MenuController::class, 'index'])->name('pengguna');
+    Route::get('/pengguna/create', [MenuController::class, 'create'])->name('pengguna.create');
+    Route::post('/pengguna/store', [MenuController::class, 'store'])->name('pengguna.store');
+    Route::get('/pengguna/edit', [MenuController::class, 'edit'])->name('pengguna.edit');
+    Route::put('/pengguna/update', [MenuController::class, 'update'])->name('pengguna.update');
+    Route::delete('/pengguna/delete', [MenuController::class, 'destroy'])->name('pengguna.delete');
+});
