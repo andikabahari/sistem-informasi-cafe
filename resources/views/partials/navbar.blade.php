d8b644550b31faceb96c23c0a8d29a41a51aa568
Andika Bahari <kzlfxx@gmail.com>
2022-01-15 19:35:27 +0700

    bold navbar link for specific route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,11 +8,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav navbar-right ml-auto">
                 @if (\App\Helpers\MyAuth::role('pemilik'))
-                    <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link nav-link-lg"><div class="d-inline-block">Dashboard</div></a></li>
-                    <li class="nav-item"><a href="{{ route('menu') }}" class="nav-link nav-link-lg"><div class="d-inline-block">Menu</div></a></li>
-                    <li class="nav-item"><a href="{{ route('pengguna') }}" class="nav-link nav-link-lg"><div class="d-inline-block">Pengguna</div></a></li>
+                    <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link nav-link-lg"><div class="d-inline-block @if (preg_match('/dashboard/', Route::currentRouteName())) font-weight-bold @endif">Dashboard</div></a></li>
+                    <li class="nav-item"><a href="{{ route('menu') }}" class="nav-link nav-link-lg"><div class="d-inline-block @if (preg_match('/menu/', Route::currentRouteName())) font-weight-bold @endif">Menu</div></a></li>
+                    <li class="nav-item"><a href="{{ route('pengguna') }}" class="nav-link nav-link-lg"><div class="d-inline-block @if (preg_match('/pengguna/', Route::currentRouteName())) font-weight-bold @endif">Pengguna</div></a></li>
                 @else
-                    <li class="nav-item"><a href="{{ route('pesanan') }}" class="nav-link nav-link-lg"><div class="d-inline-block">Pesanan</div></a></li>
+                    <li class="nav-item"><a href="{{ route('pesanan') }}" class="nav-link nav-link-lg"><div class="d-inline-block @if (preg_match('/pesanan/', Route::currentRouteName())) font-weight-bold @endif">Pesanan</div></a></li>
                 @endif
                 <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg ">
                     <div class="d-lg-inline-block"><span class="mr-1">{{ \App\Helpers\MyAuth::data()->nama_pengguna }}</span> <i class="fa fa-caret-down" style="line-height: 1; font-size: 0.75rem"></i></div></a>
